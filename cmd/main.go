cad690d79631e8464a438c4084a2d457099e9853
Bo Maryniuk <bo@suse.de>
2020-02-05 16:42:31 +0100

    Implement basic main app

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"github.com/isbm/clumad"
+	"github.com/urfave/cli/v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
 )
 
+func which(bin string, defaultPath string) string {
+	for _, envset := range os.Environ() {
+		if strings.HasPrefix(envset, "PATH=") {
+			for _, pth := range strings.Split(strings.Split(envset, "=")[1], ":") {
+				files, err := ioutil.ReadDir(pth)
+				if err != nil {
+					log.Println("Unable to access " + pth + ": " + err.Error())
+				} else {
+					for _, file := range files {
+						if bin == file.Name() {
+							return path.Join(pth, bin)
+						}
+					}
+				}
+			}
+		}
+	}
+	return path.Join(defaultPath, bin)
+}
+
+func run(ctx *cli.Context) error {
+	return nil
+}
+
 func main() {
-	op := clumad.NewSysOp()
-	op.GetSaltOps().SetSaltConfigDir("/etc/salt")
-	fmt.Println(op.GetSaltOps().GetConfOpion("minion", "master").(string))
-	fmt.Println(op.GetSaltOps().GetConfDOption("minion", "master"))
+	app := &cli.App{
+		Version: "0.1 Alpha",
+		Name:    "mgr-uccd",
+		Usage:   "Uyuni Cluster Client Daemon",
+		Action:  run,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "minion",
+				Aliases: []string{"m"},
+				Usage:   "path to Salt Minion executable",
+				Value:   which("salt-minion", "/usr/bin"),
+			},
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
