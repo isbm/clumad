91f40291906ec9fbabe362191a417860d8c66da6
Bo Maryniuk <bo@suse.de>
2020-02-05 16:07:03 +0100

    Add pinger

---
+++ pinger.go
@@ -0,0 +1,33 @@
+package clumad
+
+import (
+	"github.com/sparrc/go-ping"
+)
+
+// Pinger object
+type Pinger struct {
+	pinger *ping.Pinger
+}
+
+// Constructor
+func NewPinger() *Pinger {
+	p := new(Pinger)
+	return p
+}
+
+// Ping method pings a network address and returns statistics
+func (p *Pinger) Ping(addr string) *ping.Statistics {
+	var err error
+	var stats *ping.Statistics
+	if p.pinger == nil {
+		p.pinger, err = ping.NewPinger(addr)
+		if err != nil {
+			panic("Unabpe to initialise pinger to the address " + addr + ": " + err.Error())
+		}
+		p.pinger.Count = 3
+		p.pinger.SetPrivileged(true)
+		p.pinger.Run()
+		stats = p.pinger.Statistics()
+	}
+	return stats
+}
