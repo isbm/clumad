449c80c1907d14a8dfd16edfc214a089b85c4b58
Bo Maryniuk <bo@suse.de>
2020-02-05 19:04:56 +0100

    Fix typo

--- pinger.go
+++ pinger.go
@@ -22,7 +22,7 @@ func (p *Pinger) Ping(addr string) *ping.Statistics {
 	if p.pinger == nil {
 		p.pinger, err = ping.NewPinger(addr)
 		if err != nil {
-			panic("Unabpe to initialise pinger to the address " + addr + ": " + err.Error())
+			panic("Unable to initialise pinger to the address " + addr + ": " + err.Error())
 		}
 		p.pinger.Count = 3
 		p.pinger.SetPrivileged(true) // Still needs a raw socket capability to be set
